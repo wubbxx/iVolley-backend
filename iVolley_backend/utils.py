f5ef09b63ae46fdbaf5b00e701c74cf1ce941ca9
amor-mio-de-mi-vida <1300657172@qq.com>
2024-04-29 10:09:08 +0800

    修改了邀请码中包含的字母

--- iVolley_backend/utils.py
+++ iVolley_backend/utils.py
@@ -24,7 +24,7 @@ def generate_random_str(randomlength=8):
     生成一个指定长度的随机字符串
     """
     random_str = ''
-    base_str = 'ABCDEFGHIGKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz0123456789'
+    base_str = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghigkmnopqrstuvwxyz123456789'
     length = len(base_str) - 1
     for i in range(randomlength):
         random_str += base_str[random.randint(0, length)]
